0d7728a70376f0775e2186acba2f9a28d8128a66
puttetham <patrikhogler@hotmail.com>
2017-01-19 11:47:14 +0100

    update

--- projekt/pages/portfolio.php
+++ projekt/pages/portfolio.php
@@ -4,26 +4,26 @@
 	<ul class="portfolio-grid">
     <h3 id="git">My projects</h3>
 		<li>
-			<a href="http://williamnystrom.se/geten" class="animated flipInX">
+			<a href="http://williamnystrom.se/geten">
 				<img src="image/geten.jpg" alt="img01"/>
         <p class="jobedu">We build a simple Bed & Breakfast hotel site as a school project. In this project we used
         the languages HTML, CSS, JavaScript and PHP.</p>
 			</a>
 		</li>
     <li>
-			<a href="#" class="animated flipInX">
+			<a href="#">
 				<img src="http://placehold.it/200x200/e74c3c/FFFFFF" alt="img01"/>
           <p class="jobedu"> Upcoming project.</p>
 			</a>
 		</li>
     <li>
-			<a href="#" class="animated flipInX">
+			<a href="#">
 				<img src="http://placehold.it/200x200/8e44ad/FFFFFF" alt="img01"/>
         <p class="jobedu"> Upcoming project.</p>
 			</a>
 		</li>
     <li>
-			<a href="#" class="animated flipInX">
+			<a href="#">
 				<img src="http://placehold.it/200x200/2980b9/FFFFFF" alt="img01"/>
         <p class="jobedu"> Upcoming project.</p>
 			</a>
